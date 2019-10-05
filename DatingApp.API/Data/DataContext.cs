fcc4704f5d8c53ff74868246a163a468ed9235cf
Andrei Codreanu <andrei.codreanu94@gmail.com>
2019-10-05 11:19:01 +0300

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
