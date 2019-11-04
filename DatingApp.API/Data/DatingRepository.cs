6b2815bb244a706aeb00719fab5951cf931a8494
Andrei Codreanu <andrei.codreanu94@gmail.com>
2019-11-04 16:48:44 +0200

    section 16 work

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -109,5 +109,42 @@ namespace DatingApp.API.Data
         {
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
+        {
+            var messages = _context.Messages
+                .Include(u => u.Sender)
+                    .ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient)
+                    .ThenInclude(u => u.Photos)
+                .AsQueryable();
+
+                switch (messageParams.MessageContainer) {
+                    case "Inbox":
+                        messages = messages.Where(u => u.RecipientId == messageParams.UserId);
+                        break;  
+                    case "Outbox":
+                        messages = messages.Where(u => u.SenderId == messageParams.UserId);
+                        break;
+                    default:
+                        messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+                        break;
+                }
+
+                messages = messages.OrderByDescending(d => d.MessageSent);
+
+                return await PagedList<Message>.CreateAsync(messages,
+                    messageParams.PageNumber, messageParams.PageSize);
+        }
+
+        public Task<IEnumerable<Message>> GetMessageThread(int user, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
